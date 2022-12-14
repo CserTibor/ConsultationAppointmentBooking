c2db729c84ad2255bf4e7cbf866d1ba8176be5bc
Jay / Zsolt T <github@gain.hu>
2022-12-14 10:11:02 +0100

    style(frontend/userlist): datatable added

--- resources/views/user-list.blade.php
+++ resources/views/user-list.blade.php
@@ -11,32 +11,80 @@
 
  <section class="py-5">
         <div class="container my-5">
+            <h1>Felhasználók listája</h1>
 
-            <div>
-                @foreach($users as $user)
-                    <h3>Név: {{$user->name}}</h3>
-                     @if(auth()->user()->isAdmin())
-                        <a href="users/{{$user->id}}">
-                            Szerkesztés
-                        </a>
-                    @endif
-                    <p>Email: {{$user->email}}</p>
-                    <p>Neptun: {{$user->code}}</p>
-                    @if(!is_null($user->contact))
-                        <p>Elérhetőség: {{$user->contact}}</p>
-                    @endif
-                    <p>Jogosultságok:</p>
-                    <ul>
-                        @foreach($user->roles as $role)
-                            <li>{{$role->name}}</li>
-                        @endforeach
-                    </ul>
-                @endforeach
+            
+            
+            <div class="container">
+
+                @php
+
+                    if(count($users)==0) {
+                        echo "Nincsenek megjeleníthető felhasználók.";
+                    }
+
+                @endphp
+
+                <table class="table table-striped" id="userList">
+                  <thead>
+                    <tr>
+                      <th scope="col">Név</th>
+                      <th scope="col">Email-cím</th>
+                      <th scope="col">Azonosító</th>
+                      <th scope="col">Elérhetőség</th>
+                      <th scope="col">Jogosultságok</th>
+
+                      @if($isAdmin)
+                         <th scope="col">&nbsp;</th>
+                      @endif
+                    </tr>
+                  </thead>
+                  <tbody>
+            
+                    @foreach($users as $user)
+                        <tr>
+                            <td>
+                                {{$user->name}}
+                            </td>
+                            <td>
+                                {{$user->email}}
+                            </td>
+                            <td>
+                                {{$user->code}}
+                            </td>
+                            <td>
+                                @if(!is_null($user->contact))
+                                    {{$user->contact}}
+                                @endif
+                            </td>
+                            <td>
+                                <ul>
+                                    @foreach($user->roles as $role)
+                                        <li>{{$role->name}}</li>
+                                    @endforeach
+                                </ul>
+                            </td>
+                            <td>
+                            @if($isAdmin)
+                                <a href="users/{{$user->id}}">Szerkesztés</a>&nbsp; 
+                            @endif
+                            </td>
+                        </tr>
+                    @endforeach
+
+                    </tbody>
+                </table>
             </div>
         </div>
 </section>
 
     @include('footer')
 
+    <script>
+        $(document).ready(function () {
+          $('#userList').DataTable({"language": {"url": "//cdn.datatables.net/plug-ins/1.13.1/i18n/hu.json"}});
+          $('.dataTables_length').addClass('bs-select');
+        });
+    </script>
 </body>
 </html>
