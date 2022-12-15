101c3b509a4f37ac83855aa6d452d5e4a097411b
Jay / Zsolt T <github@gain.hu>
2022-12-15 12:13:25 +0100

    fix(frontend): upon successful login, user won't stay on login page but gets redirected

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
             return View::make('login');
         }
 
-        return View::make('user-profile', ['user' => auth()->user()]);
+        return redirect('/users/appointments');
     }
 
 
